d39115c20d44cf089485da441d629bf98d818154
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:20:46 +0530

    Add files via upload
    Write a PHP script to merge two arrays using the array_merge() function and display the
    combined result.

---
+++ task_3/script_32.php
@@ -0,0 +1,5 @@
+<?php
+$arrayis=array("india"=>"rajkot","france"=>"paris","england"=>"london");
+$country_name=array("india","france","england");
+print_r(array_merge($city_name,$country_name));
+?>
