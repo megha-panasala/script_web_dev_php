2b13fe9f934951ced1bbfd591d177d8d4fea266f
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:42:34 +0530

    Add files via upload
    Develop a PHP script that uses the array_key_first() and array_key_last() functions to
    retrieve the first and last keys of an array.

---
+++ task_3/script_55.php
@@ -0,0 +1,7 @@
+<?php
+$array1= array(4,5,98,4,36,2,545,66);
+    echo "First key element ";
+        print_r(array_key_first($array1));
+    echo "<br> Last key element ";
+        print_r(array_key_last($array1));        
+?>
