ac789459131889b9dbf230d4cefa8bf7fa7aa786
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:40:22 +0530

    Add files via upload
    Develop a PHP script to merge two associative arrays using the array_merge() function
    and display the combined result.

---
+++ task_3/script_52.php
@@ -0,0 +1,9 @@
+<?php
+$array1= array("N"=>"megha",
+                "A"=>23,
+                "c"=>"MCA");
+$array2 =array("Name"=>"shivani",
+                "Age"=>25,
+                "course"=>"MBA");
+print_r(array_merge($array1,$array2));
+?>
