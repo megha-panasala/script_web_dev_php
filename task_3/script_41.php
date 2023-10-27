d830d56bc343d0819ce3de9d7d164b40911f4139
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:31:41 +0530

    Add files via upload
    Write a PHP script that finds the intersection of two arrays using the array_intersect()
    function.

---
+++ task_3/script_41.php
@@ -0,0 +1,7 @@
+<?php
+$name=array("megha","shivani","malvi","vishva");
+$name2=array("nidhi","krushi");
+echo"<br/>";
+$intersection_value=array_intersect($name,$name2);
+print_r($intersection_value);
+?> 
