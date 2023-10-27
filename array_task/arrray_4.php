0951a30f6f5b3e17835c443735b4dba9c0560026
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 21:19:05 +0530

    Add files via upload
    4. Check if the color "Green" exists in the array from question 1.

---
+++ array_task/arrray_4.php
@@ -0,0 +1,11 @@
+<?php
+$fav_colors=array('pink','white','black','brown');
+if(in_array("green",$fav_colors))
+{
+    echo"green color is present in the arrray";
+}
+else 
+{
+     echo"green color is not present in the arrray";
+}
+?>
