19a4eabe32bbab8a472d9799bf1877a0fa5a9645
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 21:30:00 +0530

    Add files via upload
    Create a multidimensional array to store information about movies, including title, director, and
    release year for at least two movies. Display the information for one of the movies.

---
+++ array_task/array_15.php
@@ -0,0 +1,5 @@
+<?php
+$movies=array("title"=>"housfull5","directer"=>"tarun mansukhabni","releasedate"=>"1-nov-2023");
+    array("title"=>"UT69","directer"=>"rajkundra","releasedate"=>"3=nov-2023");
+print_r($movies["title"]);
+?>
