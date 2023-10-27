c684e5c8109d4b8101004401f643eae3ee8e54a2
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:32:27 +0530

    Add files via upload
     Create a PHP script that finds the difference between two arrays using the array_diff()
    function.

---
+++ task_3/script_42.php
@@ -0,0 +1,7 @@
+<?php
+$name=array("megha","shivani","vishva");
+$name2=array("smith","michael","rahul");
+echo"<br/>";
+$intersection_value=array_diff($name,$name2);
+print_r($intersection_value);
+?>
