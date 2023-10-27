fb72d0e663b569f5f6a7899a97bfdbf407a5a00e
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2023-10-27 16:41:03 +0530

    Add files via upload
    Write a PHP script that removes duplicate values from an associative array using the
    array_unique() function.

---
+++ task_3/script_53.php
@@ -0,0 +1,11 @@
+<?php
+$array1= array("N"=>"megha",
+                "A"=>21,
+                "c"=>"MCA",
+                "d"=>"MCA");
+print_r($array1);
+echo "  <br>
+        After Using the function ";
+print_r(array_unique($array1));
+
+?>
